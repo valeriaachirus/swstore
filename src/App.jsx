9e00ecc495c74a618567e1c15cd5dc9b1ac5edd7
Chizim Cezar <chizimcezar@usa.com>
2022-07-28 13:47:25 +0300

    Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
-import { About } from "./pages/About"
+import   Slider  from "./pages/Slider"
 import { Navbar } from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<Slider />} />
         </Routes>
       </Container>
     </ShoppingCartProvider>
