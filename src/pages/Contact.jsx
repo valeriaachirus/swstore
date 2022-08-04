872bf17152d010337b02691be3caf2e06d5ebdc5
valeriaachirus03@gmail.com <valeriaachirus01@gmail.com>
2022-08-04 16:54:05 +0300

    text

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './contact.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import Registration from './Components/Registration';
+import Registration from './components/Registration';
 
 
 function Contact() {
